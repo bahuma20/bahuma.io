4a1af378e58bb5074fc58f49a0fdf175d7f0aeec
Max Bachhuber <max.bachhuber@bahuma.io>
2023-01-31 02:46:48 +0100

    Fix date format

--- index.php
+++ index.php
@@ -2,17 +2,36 @@
 
 require 'vendor/autoload.php';
 
-if (!setlocale(LC_ALL, 'de_DE.utf8')) {
-    if (!setlocale(LC_ALL, 'de_DE')) {
-        error_log("Cannot set language");
-    }
-}
-
 $loader = new Twig\Loader\FilesystemLoader(dirname(__FILE__));
 $twig = new Twig\Environment($loader, [
     'cache' => dirname(__FILE__) . '/cache',
     'debug' => str_contains($_SERVER['HTTP_HOST'], 'localhost'),
 ]);
+
+$mDateFilter = new Twig\TwigFilter('mdate', function ($input) {
+   $time = strtotime($input);
+
+   $months = [
+       'Jan',
+       'Feb',
+       'Mär',
+       'Apr',
+       'Mai',
+       'Jun',
+       'Jul',
+       'Aug',
+       'Sep',
+       'Okt',
+       'Nov',
+       'Dez',
+   ];
+
+    $monthIndex = intval(date('n', $time)) - 1;
+
+    return $months[$monthIndex] . ' ' . date('Y', $time);
+});
+
+$twig->addFilter($mDateFilter);
 
 $template = $twig->load('index.html.twig');
 
